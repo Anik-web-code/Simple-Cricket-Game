551e966178685c54402a5c509412e5c31485c66d
Anik-web-code <alianik11star@gmail.com>
2025-02-02 22:07:51 +0600

    Added better script

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const options = ["dot","one","two","three","four","six"];
 let total=0;
 let balls=0;
 let highest=0;
+let name= prompt("Enter Your Name");
 
 let score = document.querySelector("#score");
 let run = document.querySelector("#run");
@@ -11,10 +12,11 @@ let feedback = document.querySelector("#Feedback");
 
 
 const genCompChoice = () => {
-  const options = ["dot","one","two","three","four","six"];
-  const randIdx = Math.floor(Math.random()*8);
+  const options = ["dot", "one", "two", "three", "four", "four", "six", "six", "six"];
+  const randIdx = Math.floor(Math.random() * options.length+1);
   return options[randIdx];
-}
+};
+
 
 const Dismissed = () =>{
       score.innerText = "Out";
@@ -22,8 +24,8 @@ const Dismissed = () =>{
       
       
       feedback.innerText = `You Are Out 
-                            Score:${total}(${balls+1})
-                            Strike Rate- ${(total*100)/(balls+1)}
+                            ${name}  ${total}(${balls+1})
+                            Strike Rate- ${((total*100)/(balls+1)).toFixed(2)}
                             Highest Score- ${highest}
                             Scroll Down for another round`;
       balls = 0;
@@ -53,28 +55,28 @@ else{
   
        if(userChoice == "one"){
            total += 1 ;
-           feedback.innerText = "Elegant Push On the on side for a single";
+           feedback.innerText = `${name} Pushes it On the on side for a single`;
        }
 
        else if(userChoice == "two"){
         total += 2;
-        feedback.innerText = "Nice punch into the gap for a couple";
+        feedback.innerText = `${name} punches it into the gap for a couple`;
 
        }
        else if(userChoice == "three"){
         total += 3;
-        feedback.innerText = "Nicely Played Almost went to boundary! but denied by some excellent fielding";
+        feedback.innerText = `Nicely Played by ${name} Almost went to boundary! but denied by some excellent fielding`;
        }
        else if(userChoice == "four"){
         total +=4;
-        feedback.innerText = "Excellent Stroke for a Four";
+        feedback.innerText = `Excellent Stroke from ${name} for a Four`;
        }
        else if(userChoice == "six"){
         total +=6;
-        feedback.innerText = "That went high and handsome the classic Mohammad Anik Ali maximum";
+        feedback.innerText = `That went high and handsome the classic ${name} maximum`;
        }
        else if(userChoice == "dot" ){
-        feedback.innerText = "You Just Commited a Crime";
+        feedback.innerText = `${name} Just Commited a Crime`;
        }
 
        run.innerText = total;
